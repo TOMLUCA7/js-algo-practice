59e10fbfbbd1f00035f1ba09672e0cc1256ed13b
TOMLUCA7 <tomluca937@gmail.com>
2026-03-02 12:11:48 +0200

    feat: implement logic to find a missing letter in a sequence of characters.

--- Medium/missingLetter.js
+++ Medium/missingLetter.js
@@ -16,8 +16,17 @@ Notes
 The given string will never have more than one missing letter.
 */
 
-function missingLetter( /*args*/ ) {
-  //your code
+function missingLetter(letters) {
+  const sortLetters = letters.split("").sort();
+
+  for (let i = 0; i < sortLetters.length - 1; i++) {
+    if (sortLetters[i + 1].charCodeAt(0) - sortLetters[i].charCodeAt(0) > 1) {
+      const missingLetter = sortLetters[i].charCodeAt(0) + 1;
+      return String.fromCharCode(missingLetter);
+    }
+  }
+
+  return "No Missing Letter";
 }
 
 exports.solution = missingLetter;
